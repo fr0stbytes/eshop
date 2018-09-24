e0fc61833a58fa94ebe8662334ef4998f5b0b7e2
fr0stbytes <m.vaporidi@gmail.com>
2018-09-24 22:52:42 +0300

    Initial Setup

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <link rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+        integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+        crossorigin="anonymous">
+    <b-navbar variant="dark" type="dark">
+      <b-link class="navbar-brand" :to="'/'">Online Store</b-link>
+      <b-navbar-nav class="ml-auto">
+        <b-link class="navbar-brand" :to="'/cart'"><i class="fas fa-shopping-cart"></i><b-badge pill variant="primary">4</b-badge></b-link>
+
+      </b-navbar-nav>
+    </b-navbar>
     <router-view/>
+    <div class="footer">
+      A Vue.js experiment by Fr0stbytes
+    </div>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  .footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    background: var(--gray-dark);
+    color: var(--gray);
+    padding: 12px 3em;
+    text-align: center;
   }
-}
 </style>
---
+++ src/components/products.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="product-list mt-5">
+      <h3>Select your favourite movie</h3>
+      <ul class="mt-3">
+        <li v-for="product in products" :key="product.id" class="w-25 p-3 mb-1">
+          <b-card :title="product.title"
+              :sub-title="product.price + ' &euro;'"
+              style="max-width: 20rem;">
+          <b-button @click="addToCart(product.id, product.title, product.price)" variant="outline-success">Add to Cart</b-button>
+      </b-card>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'products',
+  computed: mapState({
+    products: state => state.products.all
+  }),
+  methods: {
+    addToCart (id, title, price) {
+      console.log('Add to cart' + ' ' + id + ' ' + title + ' ' + price)
+    }
+  }
+}
+
+</script>
+<style lang="scss" scoped>
+  li {
+    display: inline-block;
+  }
+  .product-list {
+    text-align: center;
+  }
+</style>
